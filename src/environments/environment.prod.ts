cb0a99fc546c67cfbe907476682721b2d54e5298
ali509 <39634565+ali509@users.noreply.github.com>
2018-06-01 13:39:23 +0530

    Files uploaded

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBAp9hPNwpvG5thNMdVufuI1UE44iP1qJ0",
+    authDomain: "employee-management-1c086.firebaseapp.com",
+    databaseURL: "https://employee-management-1c086.firebaseio.com",
+    projectId: "employee-management-1c086",
+    storageBucket: "",
+    messagingSenderId: "227699858353"
+  }
+};
